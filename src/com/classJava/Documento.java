ba3de3f560b0bc67684be98c97861cea07c4d0d9
D Marques <diogomarquesbhdm19@gmail.com>
2022-04-24 20:44:18 -0300

    criação da classe documento

--- src/com/classJava/Documento.java
+++ src/com/classJava/Documento.java
@@ -3,7 +3,10 @@ package com.classJava;
 public class Documento {
     public int Id;
     public String titulo;
-    public String urlDoc;
+    //public String urlDoc; implementar caso se decidar utilizar documentos da web
 
-
+    public Documento (int Id, String titulo) {
+        this.Id = Id;
+        this.titulo = titulo;
+    }
 }
