da3a8d5499760991bf37b74ac81914eb6d667d6b
Lucas Lima <fstudies.lls@gmail.com>
2022-04-28 23:28:40 -0300

    Criacao_da_lista_dupla

--- src/classes/Elemento.java
+++ src/classes/Elemento.java
@@ -1,8 +1,15 @@
 package classes;
+
 /**
- * Nó que auxilia as verificações da lista
- *Em desenvolvimento
+ * Nó que auxilia as verificações da lista,
+ * ele gerencia os ponteiros
+ * 
+ * fonte: https://www.youtube.com/watch?v=gx8LtrOC278
+ * https://www.youtube.com/watch?v=oDAfNY_duZQ
  */
-public interface Elemento {
+public class Elemento {
+    public Elemento proximo = null;// ponteiro para o elemento anterior
+    public Elemento anterior = null; // ponteiro para o proximo elemento
+    public Termo dado = null; // dado que será armazenado dentro do elemento (espaço na memoria)
 
 }
---
+++ src/classes/Ordenacao.java
@@ -0,0 +1,5 @@
+package classes;
+
+public class Ordenacao {
+  /**não sei se iremos usar, mas se formos iremos implementar...    VER COM O CARAM */
+}
